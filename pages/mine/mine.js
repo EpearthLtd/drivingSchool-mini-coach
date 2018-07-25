fb07d166bc3b394fb3605437478bfce6a81080b8
郑维一 <weiyi@zhengweiyi.cn>
2018-07-25 16:45:24 +0800

    添加 顶部个人信息显示及样式
    添加 flex交叉轴居中和性别图标等样式

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,3 +1,14 @@
+/*!
+ * 学车微信小程序教练端 (https://github.com/EpearthLtd/aganjiaolian)
+ * 版权所有(C)2018 成都曦璞科技有限公司
+ *
+ * 作者author：郑维一
+ * 公司网站site：www.epearth.com
+*/
+
+//获取应用实例
+const app = getApp()
+
 Page({
 
   /**
@@ -28,7 +39,32 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
   },
 
   /**
